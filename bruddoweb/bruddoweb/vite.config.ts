94d8b2198b5553098c7b8bfc123f8010ce1ca525
Jakub Bartek <jakub.bartek145@gmail.com>
2025-11-26 16:07:51 +0100

    hiding scrollbar

--- bruddoweb/bruddoweb/vite.config.ts
+++ bruddoweb/bruddoweb/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/bruddoweb/', // nastavte podľa názvu repozitára
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
